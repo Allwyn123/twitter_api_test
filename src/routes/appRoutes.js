e807a66af63c725814642f832b853480eeec8bc5
Allwyn <allwyns@axioned.com>
2022-03-29 17:27:45 +0530

    user profile route added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
-//const dependencies = require('./routesDependencies').default;
+const tool = require("../../tool");
+const { User } = require('../models/userModel');
+const { appController } = require('./routesDependencies').default;
+
+router.get("/profile", (req, res) => {
+    if(req.session.user) {
+        tool.get_doc().then(user_data => {
+            user_data.find(e => req.session.user.user_name == e.user_name);
+            if(user_data) {
+                const disp = tool.display_doc(user_data.user_name);
+                appController.send_func(disp, res);
+    
+            } else {
+                appController.error_func(404, res);
+            }
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
 
 module.exports = router;
