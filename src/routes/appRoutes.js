590d559ed32e21593b9f9b3b674a5ad3a38aa5bd
Allwyn <allwyns@axioned.com>
2022-03-29 18:43:47 +0530

    update user profile added and fixed fetch data

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,19 +1,41 @@
 const router = require('express').Router();
 const tool = require("../../tool");
-const { User } = require('../models/userModel');
 const { appController } = require('./routesDependencies').default;
 
 router.get("/profile", (req, res) => {
     if(req.session.user) {
         tool.get_doc().then(user_data => {
-            user_data.find(e => req.session.user.user_name == e.user_name);
-            if(user_data) {
-                const disp = tool.display_doc(user_data.user_name);
+            const udata = user_data.find(e => req.session.user.user_name == e.user_name);
+            if(udata) {
+                const disp = tool.display_doc(udata.user_name);
                 appController.send_func(disp, res);
     
             } else {
                 appController.error_func(404, res);
             }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+router.put("/profile", (req, res) => {
+    if(req.session.user) {
+        tool.get_doc().then(user_data => {
+            const udata = user_data.find(e => req.session.user.user_name == e.user_name);
+            if(udata) {
+                const update = tool.update_doc(udata.user_name, req.body);
+                appController.send_func(update, res);
+            } else {
+                appController.error_func(404, res);
+            }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
         });
 
     } else {
