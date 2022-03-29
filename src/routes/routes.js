54c5dd31c45ed8934f6d9205dc44c30f90d57437
Allwyn <allwyns@axioned.com>
2022-03-29 12:14:09 +0530

    initial commit

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// const appRoutes = require('./appRoutes');
+const authRoutes = require('./authRoutes');
+// const passport = require('passport');
+const dependencies = require('./routesDependencies').default;
+
+
+router.get('/health', dependencies.serverHealth.checkHealth);
+
+// router.use('/app', passport.authenticate('jwt', { session : false }), appRoutes);
+router.use('/auth', authRoutes);
+
+module.exports = router;
