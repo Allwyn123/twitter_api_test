a9e358e60b875a2ff18d3766c1c7e14ea53dbec5
Allwyn <allwyns@axioned.com>
2022-03-29 17:29:59 +0530

    appRoutes added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const appRoutes = require('./appRoutes');
+const appRoutes = require('./appRoutes');
 const authRoutes = require('./authRoutes');
 // const passport = require('passport');
 const dependencies = require('./routesDependencies').default;
@@ -9,5 +9,6 @@ router.get('/health', dependencies.serverHealth.checkHealth);
 
 // router.use('/app', passport.authenticate('jwt', { session : false }), appRoutes);
 router.use(authRoutes);
+router.use(appRoutes);
 
 module.exports = router;
