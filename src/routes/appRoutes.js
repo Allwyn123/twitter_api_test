ddd3b943f159efe18139ad250e1db061ad4fa317
allwyns <allwyns@axioned.com>
2022-03-30 10:52:03 +0530

    tweets route are added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -58,7 +58,121 @@ router.delete("/profile", (req, res) => {
         }).catch((err) => {
             console.error('error', err.stack);
             appController.error_func(500, res);
-        })
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+// tweets routes
+router.post("/tweets", (req, res) => {
+    if(req.session.user) {
+        tool.get_user().then(user_data => {
+            const udata = user_data.find(e => req.session.user.user_name == e.user_name);
+            const create = tool.create_tweet(req.body, udata.user_name);
+            appController.send_func(create, res);
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+router.get("/tweets", (req, res) => {
+    if(req.session.user) {
+        tool.get_tweet().then(tweet_data => {
+            const udata = tweet_data.find(e => req.session.user.user_name == e.user_name);
+            if(udata) {
+                const disp = tool.display_tweet(udata.user_name);
+                appController.send_func(disp, res);
+    
+            } else {
+                appController.error_func(404, res);
+            }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+router.get("/tweets/:id", (req, res) => {
+    if(req.session.user) {
+        tool.get_tweet().then(tweet_data => {
+            const udata = tweet_data.find(e => req.session.user.user_name == e.user_name);
+            const para_id = parseInt(req.params.id);
+            const u_id = tool.tweet_match(req, para_id, tweet_data);
+
+            if(udata) {
+                if(u_id) {
+                    const disp = tool.display_tweet(udata.user_name, para_id);
+                    appController.send_func(disp, res);
+                } else appController.error_func(401, res);
+    
+            } else {
+                appController.error_func(404, res);
+            }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+router.put("/tweets/:id", (req, res) => {
+    if(req.session.user) {
+        tool.get_tweet().then(tweet_data => {
+            const udata = tweet_data.find(e => req.session.user.user_name == e.user_name);
+            const para_id = parseInt(req.params.id);
+            const u_id = tool.tweet_match(req, para_id, tweet_data);
+
+            if(udata) {
+                if(u_id) {
+                    const update = tool.update_tweet(udata.user_name, para_id, req.body);
+                    appController.send_func(update, res);
+                } else appController.error_func(401, res);
+            } else {
+                appController.error_func(404, res);
+            }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
+
+    } else {
+        appController.error_func(401, res);
+    }
+});
+
+
+router.delete("/tweets/:id", (req, res) => {
+    if(req.session.user) {
+        tool.get_tweet().then(tweet_data => {
+            const udata = tweet_data.find(e => req.session.user.user_name == e.user_name);
+            const para_id = parseInt(req.params.id);
+            const u_id = tool.tweet_match(req, para_id, tweet_data);
+
+            if(udata) {
+                if(u_id) {
+                    const del = tool.delete_tweet(udata.user_name, para_id);
+                    appController.send_func(del, res);
+                } else appController.error_func(401, res);
+            } else {
+                appController.error_func(404, res);
+            }
+        }).catch((err) => {
+            console.error('error', err.stack);
+            appController.error_func(500, res);
+        });
 
     } else {
         appController.error_func(401, res);
