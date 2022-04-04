21eb67e2c31240df46e9866c7acbbe17cc2ab80f
Allwyn <allwyns@axioned.com>
2022-04-04 19:17:50 +0530

    userprofile routes changed

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,27 +1,11 @@
 const router = require('express').Router();
 const tool = require("../../tool");
 const { appController } = require('./routesDependencies').default;
+const cookieParser = require("cookie-parser");
 
-router.get("/profile", (req, res) => {
-    if(req.session.user) {
-        tool.get_user().then(user_data => {
-            const udata = user_data.find(e => req.session.user.user_name == e.user_name);
-            if(udata) {
-                const disp = tool.display_user(udata.user_name);
-                appController.send_func(disp, res);
-    
-            } else {
-                appController.error_func(404, res);
-            }
-        }).catch((err) => {
-            console.error('error', err.stack);
-            appController.error_func(500, res);
-        });
+router.use(cookieParser());
 
-    } else {
-        appController.error_func(401, res);
-    }
-});
+router.get("/profile", appController.token_check, appController.userProfile);
 
 router.put("/profile", (req, res) => {
     if(req.session.user) {
