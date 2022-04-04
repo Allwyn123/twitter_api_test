2012539ff8cf2658b180bc33676da68762bcd778
Allwyn <allwyns@axioned.com>
2022-04-04 16:31:20 +0530

    jwt Credentials

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,13 @@ let noSqlDbConfig = {
 config.db = { noSqlDbConfig };
 
 
+/* JWT Authentication Credentials  */
+config.jwt = {
+  secret: process.env.JWT_SECRET  || 'prdxn',
+  expireIn: process.env.JWT_EXPIRE_IN || '1d',
+  algorithm: process.env.JWT_ALGORITHM || 'HS256',
+};
+
 config.client = process.env.CLIENT_URL || '*';
 
 module.exports = config;
