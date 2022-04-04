fa98067373b150567cc05eb74261ee5702a2b221
Allwyn <allwyns@axioned.com>
2022-04-04 17:42:28 +0530

    removed commented codes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,45 +1,5 @@
 const router = require('express').Router();
 const { authClient } = require('./routesDependencies').default;
-const tool = require("../../tool");
-const bcrypt = require("bcrypt");
-const body_parser = require("body-parser");
-const session = require('express-session');
-
-router.use(body_parser.json());
-router.use(session({secret: "mySession", resave: true, saveUninitialized: false}));
-
-// /**
-//  * @swagger
-//  * /auth/login:
-//  *  post:
-//  *    tags:
-//  *      - Authentication
-//  *    name: Local Login API
-//  *    summary: Based on user's data, this api sent jwt token which leads to login process.
-//  *    consumes:
-//  *      - application/json
-//  *    produces:
-//  *      - application/json
-//  *    parameters:
-//  *      - name: Body Data
-//  *        in: body
-//  *        schema:
-//  *         type: object
-//  *         properties:
-//  *          email: 
-//  *            type: string
-//  *          password:
-//  *            type: string
-//  *        required:
-//  *         - email
-//  *         - password
-//  *    responses:
-//  *      200:
-//  *        description: JWT token will be in response.
-//  *      500:
-//  *        description: Internal server error.
-//  */
-// router.post('/login', dependencies.authClient.login);
 
 /**
  * @note All routes regarding local signup OR using Oauth sign-in should be listed below. 
