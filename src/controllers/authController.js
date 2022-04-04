5b368960ab054dff8932aa7fdc6368eadabdb533
Allwyn <allwyns@axioned.com>
2022-04-04 16:25:15 +0530

    login controller added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-// const authService = require('../services/authServices');
+const authService = require('../services/authServices');
 const errorMsg = require('../helpers/errorMessage').errorMessages;
 const utils = require('../helpers/utils');
 const { User } = require("../models/userModel");
@@ -28,29 +28,29 @@ const bcrypt = require('bcrypt');
 //   }
 // };
 
-/**
- * @description Local login controller.
- * @function login
- * @param {Status Code} status_code 
- * @param {Response} res 
- */
- exports.login = (status_code, error, msg, res) => {
-  if(error) {
-      if(status_code == 404) {
-          res.status(404).send(utils.responseMsg(msg, false, null))
-      }
+// /**
+//  * @description Local login controller.
+//  * @function login
+//  * @param {Status Code} status_code 
+//  * @param {Response} res 
+//  */
+//  exports.login = (status_code, error, msg, res) => {
+//   if(error) {
+//       if(status_code == 404) {
+//           res.status(404).send(utils.responseMsg(msg, false, null))
+//       }
       
-      if(status_code == 401) {
-          res.status(401).send(utils.responseMsg(msg, false, null))
-      }
+//       if(status_code == 401) {
+//           res.status(401).send(utils.responseMsg(msg, false, null))
+//       }
 
-      if(status_code == 500) {
-        res.status(500).send(utils.responseMsg(msg, false, null));
-      }
-  } else {
-      res.send(utils.responseMsg(null, true, msg));
-  }
-}
+//       if(status_code == 500) {
+//         res.status(500).send(utils.responseMsg(msg, false, null));
+//       }
+//   } else {
+//       res.send(utils.responseMsg(null, true, msg));
+//   }
+// }
 
 /**
  * @description Local logout controller.
@@ -71,48 +71,64 @@ const bcrypt = require('bcrypt');
     console.error('error', err.stack);
     res.status(500).send(utils.responseMsg(errorMsg.internalServerError));
    }
-}
-
-// /**
-//  * @description Local signup controller.
-//  * @function signup
-//  * @param {Promise} param 
-//  * @param {Response} res 
-//  */
-//  exports.signup = (param, res) => {
-//   param.then(e => {
-//       if(e) res.send(utils.responseMsg(null, true, e));
-//       if(!e) res.status(404).send(utils.responseMsg(errorMsg.dataNotFound, false, null));
-//   })
-//   .catch(err => res.send(utils.responseMsg(err, false, null)));
-// }
-
+};
 
 /**
  * @description Local signup controller.
  * @function signup
  */
 exports.signup = async (req, res) => {
-//   const create = tool.create_user(req.body);
-//   authClient.signup(create, res);
- 
-try {
-	const doc = req.body;
-	const userExist = await User.find({user_name: doc.user_name});
+	try {
+		const doc = req.body;
+		const userExist = await User.findOne({user_name: doc.user_name});
 
-	if(userExist == 0) {
-		doc.password = await bcrypt.hash(doc.password, 10);	
-		const mydata = new User(doc);
-		await mydata.save();
-	
-		const msg = "User Created";
-		res.status(201).send(utils.responseMsg(null, true, msg));
-	} else {
-		const msg = "User Existed";
-		res.status(409).send(utils.responseMsg(msg, false, null));
+		if(userExist == null) {
+			doc.password = await bcrypt.hash(doc.password, 10);	
+			const mydata = new User(doc);
+			await mydata.save();
+		
+			const msg = "User Created";
+			res.status(201).send(utils.responseMsg(null, true, msg));
+		} else {
+			const msg = "User Existed";
+			res.status(409).send(utils.responseMsg(msg, false, null));
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(utils.responseMsg(err.errors, false, null));
 	}
-  } catch (err) {
-    console.error(err);
-	res.status(500).send(utils.responseMsg(err.errors, false, null));
+};
+
+/**
+ * @description Local login controller.
+ * @function login
+ */
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+	const loginMatch =  await User.findOne({email});
+	const err_mess = { message: "login failed", status: "Unauthorized" };
+
+	if(loginMatch != null) {
+		const match = await bcrypt.compare(password, loginMatch.password);
+		if(match) {
+			const user = {user_name: loginMatch.user_namee};
+			const accessToken = authService.createToken(user);
+			res.cookie("jwt", accessToken);
+
+			const message = { message: `login success (${loginMatch.name})` };
+			res.send(utils.responseMsg(null, true, message));
+			
+		} else {
+			res.status(401).send(utils.responseMsg(err_mess, false, null));
+		}
+
+	} else {
+		res.status(401).send(utils.responseMsg(err_mess, false, null));
+	}
+
+  } catch (error) {
+    console.error('error', error.stack);
+    res.status(500).send(utils.responseMsg(errorMsg.internalServerError));
   }
-}
+};
