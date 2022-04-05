5b36f61b7efa82641ce44146847256a46ffcf854
Allwyn <allwyns@axioned.com>
2022-04-05 17:45:09 +0530

    sort timeline route modified

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -14,24 +14,7 @@ router.post("/profile/tweets", appController.token_check, appController.createTw
 router.get("/profile/timeline", appController.token_check, appController.timeline);
 router.put("/profile/tweets/:id", appController.token_check, appController.updateTweet);
 router.delete("/profile/tweets/:id", appController.token_check, appController.deleteTweet);
-
-
-router.get("/timeline/:sort", (req, res) => {
-    if(req.session.user) {
-        if(req.params.sort == "date") {
-            const sorted = tool.sort_func("date");
-            appController.send_func(sorted, res);
-        }
-        
-        if(req.params.sort == "like") {
-            const sorted = tool.sort_func("like");
-            appController.send_func(sorted, res);
-        }
-    } else {
-        appController.error_func(401, res);
-    }
-});
-
+router.get("/profile/timeline/:sort", appController.token_check, appController.timelineSort);
 router.put("/profile/tweets/:id/like/", appController.token_check, appController.likeTweet);
 
 module.exports = router;
