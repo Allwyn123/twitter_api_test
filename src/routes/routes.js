006a041b2c7ac745889a10edf1ed99d3c4bdc849
Allwyn <allwyns@axioned.com>
2022-04-05 18:00:10 +0530

    remove commented codes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const appRoutes = require('./appRoutes');
 const authRoutes = require('./authRoutes');
-// const passport = require('passport');
 const dependencies = require('./routesDependencies').default;
 
 
 router.get('/health', dependencies.serverHealth.checkHealth);
 
-// router.use('/app', passport.authenticate('jwt', { session : false }), appRoutes);
 router.use(authRoutes);
 router.use(appRoutes);
 
