d0c52017879bb15f384137fe60b2b197c7453c86
Allwyn <allwyns@axioned.com>
2022-04-05 18:00:40 +0530

    remove commented code and unused

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -4,38 +4,6 @@ const { User } = require("../models/userModel");
 const { Tweet } = require("../models/tweetModel");
 const jwt = require("jsonwebtoken");
 
-// /**
-//  * Send data/message as Response
-//  * @param {Promise} param 
-//  * @param {Response} res 
-//  */
-//  exports.send_func = (param, res) => {
-//     param.then(e => {
-//         if(e) res.send(utils.responseMsg(null, true, e));
-//         if(!e) res.status(404).send(utils.responseMsg(errorMsg.dataNotFound, false, null));
-//     })
-//     .catch(err => res.send(utils.responseMsg(err, false, null)));
-// }
-
-
-/**
- * Send error message as Response
- * @param {Status Code} status_code 
- * @param {Response} res 
- */
- exports.error_func = (status_code, res) => {
-    if(status_code == 404) {
-        res.status(404).send(utils.responseMsg(errorMsg.dataNotFound, false, null));
-    }
-    
-    if(status_code == 401) {
-        res.status(401).send(utils.responseMsg(errorMsg.unauthorized, false, null));
-    }
-
-    if(status_code == 500) {
-        res.status(500).send(utils.responseMsg(errorMsg.internalServerError, false, null));
-    }
-};
 
 /**
  * @description find user data.
